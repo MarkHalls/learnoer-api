cd24c9bb9cb6f8346535c6a5e6aa80e013a93464
Mark Halls <mark.halls@gmail.com>
2020-05-03 22:29:10 -0700

    Add initial migrations

---
+++ database/migrations/20200503213725_books.js
@@ -0,0 +1,13 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("books", (books) => {
+    books.uuid("id").primary();
+    books.string("isbn", 64).notNullable();
+    books.text("author").notNullable();
+    books.text("publisher");
+    books.text("title").notNullable();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("books");
+};
