9ae1721bf2fef454d08c1b62285a4601d16e5033
Mark Halls <mark.halls@gmail.com>
2020-05-06 16:24:27 -0700

    Search by text in openlibrary.org

--- routers/api/books/index.js
+++ routers/api/books/index.js
@@ -1,3 +1,10 @@
+const { v4: uuidv4 } = require("uuid");
+const axios = require("axios");
+
+const books = require("../../../database/models/books");
+
+const openLibSearch = "http://openlibrary.org/search.json?q=";
+
 module.exports = {
   name: "books",
 
@@ -6,7 +13,53 @@ module.exports = {
       method: "GET",
       path: "/",
       handler: async (req, h) => {
-        return "Dis is da books";
+        return "Da Books";
+        // return books.find();
+      },
+    });
+    server.route({
+      method: "POST",
+      path: "/",
+      handler: async (req, h) => {
+        const { isbn, title, author, publisher } = req.payload;
+
+        const book = {
+          id: uuidv4(),
+          isbn,
+          title,
+          author,
+          publisher,
+        };
+
+        try {
+          const newBook = await books.add(book);
+          return { newBook };
+          console.log(newBook);
+        } catch (err) {
+          console.log(err);
+        }
+      },
+    });
+    server.route({
+      method: "POST",
+      path: "/search",
+      handler: async (req, h) => {
+        const searchResults = await axios.get(
+          encodeURI(`${openLibSearch}${req.payload.searchTerm}`)
+        );
+
+        const available = searchResults.data.docs
+          .map((book) => {
+            if (book.availability.status !== "error") {
+              return book;
+            }
+          })
+          .filter((book) => book);
+        return {
+          numFound: searchResults.data.numFound,
+          filtered: available.length,
+          available,
+        };
       },
     });
   },
