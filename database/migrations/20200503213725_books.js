ab3fdb8ebaf1c3f21aad01e7665e22896d966a6c
Mark Halls <mark.halls@gmail.com>
2020-05-06 16:21:45 -0700

    Make isbn unique

--- database/migrations/20200503213725_books.js
+++ database/migrations/20200503213725_books.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable("books", (books) => {
     books.uuid("id").primary();
-    books.string("isbn", 64).notNullable();
+    books.string("isbn", 64).notNullable().unique();
     books.text("author").notNullable();
     books.text("publisher");
     books.text("title").notNullable();
