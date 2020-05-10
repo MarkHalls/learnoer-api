3990d6772dd2ce65b060e07ac77189ea8e929a97
Mark Halls <mark.halls@gmail.com>
2020-05-09 19:17:02 -0700

    Use same endpoint for ISBN and title search

--- routers/api/search/index.js
+++ routers/api/search/index.js
@@ -1,5 +1,5 @@
 const {
-  textSearch,
+  searchByTitle,
   searchByIsbn,
   filterAvailableBooks,
 } = require("../../../clients/openLibraryApiClient");
@@ -10,33 +10,35 @@ module.exports = {
   async register(server) {
     server.route({
       method: "GET",
-      path: "/{isbn}",
+      path: "/{term}",
       handler: async (req, h) => {
-        const { isbn } = req.params;
+        //TODO limit requests to 4094 characters
 
-        const foundBooks = await searchByIsbn([isbn]);
+        try {
+          const { term } = req.params;
 
-        if (foundBooks.length === 0) {
-          return [];
+          const foundBooks = await searchByIsbn([term]);
+
+          if (foundBooks.length === 0) {
+            //assume the path was actually a title
+            const books = await searchByTitle(term);
+            return filterAvailableBooks(books);
+          }
+
+          const availableBooks = filterAvailableBooks(foundBooks);
+
+          if (availableBooks.length === 0) {
+            const relatedBooks = await searchByTitle(foundBooks[0].data.title);
+
+            return filterAvailableBooks(relatedBooks);
+          }
+
+          return searchByIsbn([term]);
+        } catch (err) {
+          console.error(err);
+          console.error(err.data.payload.toString());
+          console.error(err.data.res.req);
         }
-        const availableBooks = filterAvailableBooks(foundBooks);
-
-        if (availableBooks.length === 0) {
-          const relatedBooks = await textSearch(foundBooks[0].data.title);
-
-          return filterAvailableBooks(relatedBooks);
-        }
-
-        return searchByIsbn([isbn]);
-      },
-    });
-
-    server.route({
-      method: "POST",
-      path: "/",
-      handler: async (req, h) => {
-        const availableBooks = await textSearch(req.payload.searchTerm);
-        return filterAvailableBooks(availableBooks);
       },
     });
   },
