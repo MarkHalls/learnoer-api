d9e69fe8e5843f4d1e554a50d62adac8449b0de8
Mark Halls <mark.halls@gmail.com>
2020-05-14 19:15:57 -0700

    Set cors to ignore origin

--- api/server.js
+++ api/server.js
@@ -9,6 +9,9 @@ const makeServer = () => {
   const server = Hapi.server({
     port: 3000,
     host: "localhost",
+    routes: {
+      cors: { origin: "ignore" },
+    },
   });
 
   return server;
