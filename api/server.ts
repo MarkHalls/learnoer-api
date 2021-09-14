10259c692076621301f2aa4575e3d5df89798733
Mark Halls <mark.halls@gmail.com>
2021-09-14 09:40:23 -0700

    Add cors

--- api/server.ts
+++ api/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import helmet from "helmet";
+import cors from "cors";
 
 import { logger } from "./middleware";
 
@@ -8,6 +9,7 @@ import routers from "../routers/api";
 const server = express();
 
 server.use(helmet());
+server.use(cors());
 server.use(logger);
 server.use(express.json());
 server.use("/api", routers);
